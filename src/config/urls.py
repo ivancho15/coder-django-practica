550d4d59ac7dd4d7de8fb983ac7cee97421cf1ec
Ivan Marcano <ijmm15@gmail.com>
2026-03-24 22:37:12 -0500

    agrega ejemplos en core

--- src/config/urls.py
+++ src/config/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core.views import index, saludar, saludar2, saludar3
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('saludar', saludar),
+    path('saludar2', saludar2),
+    path('saludar3/<str:nombre>/<str:apellido>', saludar3)
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
+def saludar(request):
+        return HttpResponse("Hola desde Django")
+
+def saludar2(request):
+        return HttpResponse("<H1>Esta es el titulo</H1>")
+
+def saludar3(request, nombre: str, apellido: str):
+        nombre = nombre.upper()
+        apellido = apellido.capitalize()
+        return HttpResponse(f"{apellido}, {nombre}")
+
+def index(request):
+        ahora = datetime.now()
+        contexto = {
+                "fecha":ahora
+        }
+        return render(request, "core/index.html", contexto)
